f730f00f6c59b32069db8bd96ebebcca530526d2
a-ayyash <ahmed.ayyash@gmail.com>
2024-05-31 00:48:12 +0300

    I don't know

---
+++ lua/ayyashio/plugins/misc.lua
@@ -0,0 +1,6 @@
+return {
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+}
--- lua/ayyashio/set.lua
+++ lua/ayyashio/set.lua
@@ -1,7 +1,12 @@
 vim.opt.guicursor = ""
+vim.opt.clipboard = "unnamedplus"
+vim.opt.showmode = false
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.g.have_nerd_font = true
+
+vim.opt.signcolumn = "yes"
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -22,10 +27,43 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+vim.opt.hlsearch = true
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("ayyashio-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
