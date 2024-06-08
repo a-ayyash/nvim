4e1b35ed234d62d0b54df2d9e7e30b1eeac43747
a-ayyash <ahmed.ayyash@gmail.com>
2024-06-08 22:56:14 +0300

    Make it mine again.

--- lua/ayyash/core/keymaps.lua
+++ lua/ayyash/core/keymaps.lua
@@ -3,27 +3,26 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk"})
-keymap.set("n", "<leader>cl", ":nohl<CR>", { desc = "Clear search highlights"})
 
+-- clean shit up
+keymap.set("n", "<leader><ESC>", function() require("notify").dismiss({ silent = true, pending = true }) vim.cmd("nohl") end, { desc = "Dismiss All Notifications" }) --  move current buffer to new tab
 
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number"})
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number"})
-
-
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically"})
-keymap.set("n", "<leader>sn", "<C-w><C-w>", { desc = "Split window vertically"})
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally"})
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size"})
-keymap.set("n", "<leader>sq", "<cmd>close<CR>", { desc = "Close current split"})
+-- enable splits
+-- keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically"})
+-- keymap.met("n", "<leader>sn", "<C-w><C-w>", { desc = "Split window vertically"})
+-- keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally"})
+-- keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size"})
+-- keymap.set("n", "<leader>sq", "<cmd>close<CR>", { desc = "Close current split"})
 -- keymap.set("n", "<leader>s<up>", "<C-w><up>", { desc = "Navigate split up" })
 -- keymap.set("n", "<leader>s<down>", "<C-w><down>", { desc = "Navigate split down" })
 -- keymap.set("n", "<leader>s<left>", "<C-w><left>", { desc = "Navigate split left" })
 -- keymap.set("n", "<leader>s<right>", "<C-w><right>", { desc = "Navigate split right" })
 
+
+-- enable tabs
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
 
--- lua/ayyash/core/options.lua
+++ lua/ayyash/core/options.lua
@@ -1,10 +1,12 @@
 vim.cmd("let g:netrw_liststyle = 3")
 
-
 local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+opt.mouse = "a"
 
 -- tabs & indentation
 opt.tabstop = 2
@@ -12,12 +14,10 @@ opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
-
 opt.wrap = false
 
 opt.ignorecase = true
 opt.smartcase = true
-
 
 opt.backspace = "indent,eol,start"
 
@@ -32,3 +32,14 @@ opt.clipboard:append("unnamedplus")
 
 opt.splitright = true
 opt.splitbelow = true
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
